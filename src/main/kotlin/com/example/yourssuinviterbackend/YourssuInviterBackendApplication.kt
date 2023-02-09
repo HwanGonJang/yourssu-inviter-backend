10e91a1cef5e61f1169bbfc1e66beb177368dc74
HwanGonJang <myggona@gmail.com>
2023-02-09 10:16:12 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/yourssuinviterbackend/YourssuInviterBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yourssuinviterbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YourssuInviterBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<YourssuInviterBackendApplication>(*args)
+}
